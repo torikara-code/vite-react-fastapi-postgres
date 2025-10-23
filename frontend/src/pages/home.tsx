39c703c23edba63cc00c098de420300e7d2a2f75
notolq <206822596+torikara-code@users.noreply.github.com>
2025-10-24 03:47:45 +0900

    Homeの文字がずれてたので修正

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,6 +1,6 @@
 const Home: React.FC = () => {
   return (
-    <div className="text-center">
+    <div className="text-center m-auto flex flex-col justify-center items-center h-full ">
       <h2 className="text-center text-4xl">Home</h2>
       <p className="text-slate-400 py-3">click! "ユーザー管理"</p>
     </div>
